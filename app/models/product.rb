accc041d316ed502f14d39dd8af144a94add6f16
wojzag <eivelloth@gmail.com>
2015-07-15 19:44:03 +0000

    Fixed tests for Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+  
+  validates :description, presence: true
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{1,2})?\z/ }
+  validates :title, presence: true
+  
+  def average_rating
+    self.reviews.average(:rating)
+  end
 end
