f1c335f1962df59dea97494d0c2cfa5e84bfbcab
wojzag <eivelloth@gmail.com>
2015-07-18 21:17:03 +0000

    Finished profile view

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "admin@workshops.pl", firstname: "Grzegorz", lastname: "Brzęczyszczykiewicz", password: "12345678")
+User.create(email: "admin@workshops.pl", firstname: "Grzegorz", lastname: "Brzęczyszczykiewicz", password: "12345678", admin: true)
